9c3be777171ca3c6a3b3c370c9dbf9a7841aa7ac
Renan Lima <renan@lima>
2020-07-14 16:37:36 -0300

    add events

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,16 @@
+
+
+new Vue({
+    el: '#desafio',
+    data:{
+        name: 'Renan Lima',
+        age: 25,
+        img: 'https://cdn.auth0.com/blog/logos/vuejs-logo.png'
+    },
+    methods:{
+        sort: function () {
+            number = Math.random()
+            return number
+        }
+    }
+})
